619a2ae192ad920dac4107a96ee7732548fa1625
balazsbarni <balazsbarni@gmail.com>
2020-06-09 18:23:40 +0200

    fix(dev-test backend): project routes add

--- backend/src/restApi/route/project.js
+++ backend/src/restApi/route/project.js
@@ -4,5 +4,9 @@ const router = express.Router()
 
 
 router.get('/', project.list)
+router.post('/', project.validationRules('create'), project.validate, project.create)
+router.get('/:id', project.read)
+router.put('/:id', project.update)
+router.delete('/:id', project.delete)
 
 export default router
