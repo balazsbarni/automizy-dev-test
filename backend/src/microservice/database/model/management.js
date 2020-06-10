e70ad5b0a6efd0d32e36e2a11316b3c945bdfe5d
balazsbarni <balazsbarni@gmail.com>
2020-06-10 10:43:48 +0200

    feat(dev-test backend): management model add

---
+++ backend/src/microservice/database/model/management.js
@@ -0,0 +1,34 @@
+const ManagementModel = ({
+  sequelize,
+  DataType
+}) => {
+  const { INTEGER, DATE, NOW } = DataType
+  const Management = sequelize.define('management', {
+    id: {
+      type: INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    student_id: {
+      type: INTEGER,
+      references: {
+        model: 'students',
+        key: 'id'
+      }
+    },
+    project_id: {
+      type: INTEGER,
+      references: {
+        model: 'projects',
+        key: 'id'
+      }
+    },
+    createdAt: {
+      type: DATE,
+      defaultValue: NOW
+    }
+  })
+  return Management;
+}
+
+export default ProjectModel
